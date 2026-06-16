eab0b649f8522ea5fb9ef6d6db2eda2e4179eb8a
Zach Glossip <zglossip@gmail.com>
2026-06-15 20:24:49 -0500

    Set up App

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+import App from './App.vue';
 import router from "./router";
 
-import { IonicVue } from "@ionic/vue";
+const app = createApp(App);
 
-/* Core CSS required for Ionic components to work properly */
-import "@ionic/vue/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
-import "@ionic/vue/css/normalize.css";
-import "@ionic/vue/css/structure.css";
-import "@ionic/vue/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
-import "@ionic/vue/css/padding.css";
-import "@ionic/vue/css/float-elements.css";
-import "@ionic/vue/css/text-alignment.css";
-import "@ionic/vue/css/text-transformation.css";
-import "@ionic/vue/css/flex-utils.css";
-import "@ionic/vue/css/display.css";
-
-/* Theme variables */
-import "./theme/variables.css";
-
-const app = createApp(App).use(IonicVue).use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
 
 router.isReady().then(() => {
   app.mount("#app");
-});
+});;
