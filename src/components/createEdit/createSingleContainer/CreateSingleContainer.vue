4cbf3e862b22114f798b32a98c03606e855d69c0
Zach Glossip <zglossip@gmail.com>
2026-06-20 17:24:40 -0500

    Convert CreateSingleContainer

--- src/components/createEdit/createSingleContainer/CreateSingleContainer.vue
+++ src/components/createEdit/createSingleContainer/CreateSingleContainer.vue
@@ -5,6 +5,12 @@ import {
   useCreateSingleContainerService,
 } from "./createSingleContainerService";
 
+import Card from "primevue/card";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import Textarea from "primevue/textarea";
+import Button from "primevue/button";
+
 const {
   name,
   coursesString,
@@ -20,72 +26,77 @@ const {
 </script>
 
 <template>
-  <ion-card>
-    <ion-card-content>
-      <ion-list>
-        <ion-item>
-          <ion-input label="Name" label-placement="stacked" v-model="name" />
-        </ion-item>
-        <ion-item>
-          <ion-input
-            label="Course(s)"
-            label-placement="stacked"
-            v-model="coursesString"
-          />
-        </ion-item>
-        <ion-item>
-          <ion-input
-            label="Cuisine(s)"
-            label-placement="stacked"
-            v-model="cuisinesString"
-          />
-        </ion-item>
-        <ion-item>
-          <ion-input
-            label="Tag(s)"
-            label-placement="stacked"
-            v-model="tagsString"
-          />
-        </ion-item>
-        <ion-item>
-          <ion-input
-            label="Serving Amount"
-            label-placement="stacked"
-            v-model="servingAmount"
-            type="number"
-          />
-        </ion-item>
-        <ion-item>
-          <ion-input
-            label="Serving Name"
-            label-placement="stacked"
-            v-model="servingName"
-          />
-        </ion-item>
-        <ion-item>
-          <ion-input
-            label="Source URL"
-            label-placement="stacked"
-            v-model="sourceUrl"
-            type="url"
-          />
-        </ion-item>
-        <ion-item>
-          <ion-textarea
-            label="Ingredients"
-            label-placement="stacked"
-            v-model="ingredientsString"
-          />
-        </ion-item>
-        <ion-item>
-          <ion-textarea
-            label="Instructions"
-            label-placement="stacked"
-            v-model="instructionsString"
-          />
-        </ion-item>
-      </ion-list>
-    </ion-card-content>
-    <ion-button fill="clear" @click="add">Add</ion-button>
-  </ion-card>
+  <Card>
+    <template #content>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-name" class="font-semibold w-36">Name</label>
+        <InputText id="rc-name" v-model="name" class="flex-auto" />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-courses" class="font-semibold w-36">Course(s)</label>
+        <InputText id="rc-courses" v-model="coursesString" class="flex-auto" />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-cuisines" class="font-semibold w-36">Cuisine(s)</label>
+        <InputText
+          id="rc-cuisines"
+          v-model="cuisinesString"
+          class="flex-auto"
+        />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-tags" class="font-semibold w-36">Tag(s)</label>
+        <InputText id="rc-tags" v-model="tagsString" class="flex-auto" />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-serving-amount" class="font-semibold w-36"
+          >Serving Amount</label
+        >
+        <InputNumber
+          id="rc-serving-amount"
+          v-model="servingAmount"
+          class="flex-auto"
+        />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-serving-name" class="font-semibold w-36"
+          >Serving Name</label
+        >
+        <InputText
+          id="rc-serving-name"
+          v-model="servingName"
+          class="flex-auto"
+        />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-source-url" class="font-semibold w-36">Source URL</label>
+        <InputText id="rc-source-url" v-model="sourceUrl" class="flex-auto" />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-ingredients" class="font-semibold w-36"
+          >Ingredients</label
+        >
+        <Textarea
+          id="rc-ingredients"
+          v-model="ingredientsString"
+          class="flex-auto"
+        />
+      </div>
+      <div class="flex items-center gap-4 mb-4">
+        <label for="rc-instructions" class="font-semibold w-36"
+          >Instructions</label
+        >
+        <Textarea
+          id="rc-instructions"
+          v-model="instructionsString"
+          class="flex-auto"
+        />
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex justify-end">
+        <Button label="Add" @click="add" />
+      </div>
+    </template>
+  </Card>
 </template>
